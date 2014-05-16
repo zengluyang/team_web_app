2418a935a6a5ba928e55711e7057c548d7947e5f
Zeng Luyang <zengluyang@gmail.com>
2014-05-16 12:51:45 +0800

    MOD: change index page layout

--- themes/foundation4/views/site/index.php
+++ themes/foundation4/views/site/index.php
@@ -4,43 +4,47 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-<ul data-orbit data-options="timer_speed:5000;
-pause_on_hover=false;
-slide_number=false;">
-  <li>
-	<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/bluetooth_street_big.png" alt="slide 1" />
-	<div class="orbit-caption">
-	  Wireless Ad Hoc Network.
+<div class="row">
+	<div class="columns medium-6">
+	<ul data-orbit data-options="timer_speed:5000;
+	pause_on_hover=false;
+	slide_number=false;">
+	  <li>
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/bluetooth_street_big.png" alt="slide 1" />
+		<div class="orbit-caption">
+		  Wireless Ad Hoc Network.
+		</div>
+	  </li>
+	  <li>
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/https-network-associates-guide-to-communications-protocols.png" alt="slide 2" />
+		<div class="orbit-caption">
+		  Network Protocols.
+		</div>
+	  </li>
+	  <li>
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/computer_networking_services.jpg" alt="slide 3" />
+		<div class="orbit-caption">
+		  Network Architecture.
+		</div>
+	  </li>
+	</ul>
 	</div>
-  </li>
-  <li>
-	<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/https-network-associates-guide-to-communications-protocols.png" alt="slide 2" />
-	<div class="orbit-caption">
-	  Network Protocols.
+	<div class="columns medium-6">
+		<p>团队目前主要从事以下五个方面的研究：</p>
+		<ul>
+			<li>网络体系结构与协议：包括NGN/NGI、移动网络通信体制、组网体系、协议架构、互联与路由技术；</li>
+			<li>无线自组织网络：包括Ad Hoc、无线Mesh、车联网等动态网络的通信技术、信道访问技术、动态组网与路由技术；</li>
+			<li>无线传感网(WSN)与物联网：包括WSN节能通信技术，节能组网协议，传感信息采集、融合、存储与汇集技术，RFID读写防碰撞技术，RFID组网与定位技术，物联网应用系统；</li>
+			<li>宽带无线移动网络技术：包括下一代WLAN技术、 4G网络、宽带无线专网、异构网络融合技术，无线资源管理技术；</li>
+			<li>无线网络新技术：包括认知无线网络、移动社会网络、智能电网、协同中继、网络级抗干扰技术、网络安全等。</li>
+		</ul>
 	</div>
-  </li>
-  <li>
-	<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/computer_networking_services.jpg" alt="slide 3" />
-	<div class="orbit-caption">
-	  Network Architecture.
-	</div>
-  </li>
-</ul>
 
-
-
-
+</div>
 <div id="about">
 <p>网络技术团队自上世纪80年代中期开始从事计算机网络、无线网络领域的研究工作，是国内开展无线网络技术研究的先行者之一。自“七五”以来，先后完成了包括军事电子预研项目、国家重大专项项目、国家863重大重点项目、国家自然基金项目、教育部重点项目，以及校企合作等数十项科研项目，取得丰硕的成果。获得多项国家级和省部级科研成果奖励。目前有教师15人，其中教授2名，副教授3名，博士4名，在职博士研究生7名。</p>
 <p>团队长期从事网络体系与协议的研究，尤其在无线自组织网络研究方面成效卓著，自主研发多种无线组网设备，自主设计并实现多种无线网络组网结构、路由协议、MAC协议并获得多项专利。技术底蕴深厚，在网络体系结构设计与分析技术、网络协议设计与协议分析技术、协议软件实现技术以及网络工程与应用技术等方面技术实力国内突出。</p>
 <p>团队创建并承担了网络工程国家特色专业的建设，承建“通信与信息系统国家级实验教学中心”骨干传输系统实验室、路由与互连实验室、网络工程实验室、综合接入网实验室等系列网络技术实验室，建设资金累计数千万元，涵盖SDH光传输网，IP网络、程控交换系统、EPON光接入网，WLAN无线接入网等，构成商用级的核心网、接入网现代网络系统平台，实验室技术国际先进，国内一流。</p>
-<p>团队目前主要从事以下五个方面的研究：
-	<ol>
-	<li>网络体系结构与协议：包括NGN/NGI、移动网络通信体制、组网体系、协议架构、互联与路由技术；</li>
-	<li>无线自组织网络：包括Ad Hoc、无线Mesh、车联网等动态网络的通信技术、信道访问技术、动态组网与路由技术；</li>
-	<li>无线传感网(WSN)与物联网：包括WSN节能通信技术，节能组网协议，传感信息采集、融合、存储与汇集技术，RFID读写防碰撞技术，RFID组网与定位技术，物联网应用系统；</li>
-	<li>宽带无线移动网络技术：包括下一代WLAN技术、 4G网络、宽带无线专网、异构网络融合技术，无线资源管理技术；</li>
-	<li>无线网络新技术：包括认知无线网络、移动社会网络、智能电网、协同中继、网络级抗干扰技术、网络安全等。</li>
-	</ol>
+
 </div>
 
